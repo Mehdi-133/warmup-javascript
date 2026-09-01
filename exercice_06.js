8c0355a99c0c54a66132435717c97a484f2a8a34
Mehdi-133 <mehdikarbitou@gmail.com>
2026-09-01 22:41:32 +0100

    exercice 6

---
+++ exercice_06.js
@@ -0,0 +1,25 @@
+const panier = [
+  { nom: "Souris", prix: 150, quantite: 2 },
+  { nom: "Casque", prix: 400, quantite: 1 },
+  { nom: "Tapis", prix: 60, quantite: 3 },
+  { nom: "Webcam", prix: 520, quantite: 1 },
+];
+
+const names = panier.map(e => e.nom)
+console.log(names);
+
+const total = panier.map(e => e.nom + " " +  e.prix * e.quantite)
+console.log(total);
+
+const filter = panier.filter(e => e.prix > 100)
+console.log(filter.map(e => e.nom));
+
+const price = panier.map(e => e.prix)
+const totalPrice = price.reduce((acc , val) => acc + val , 0)
+
+console.log(totalPrice);
+
+const quantite = panier.map(e => e.quantite)
+const totalArticle = quantite.reduce((acc , val) => acc + val , 0)
+
+console.log(totalArticle);
