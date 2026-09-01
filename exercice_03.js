c13744093b8680b464f841c5a82ae24ab8da7953
Mehdi-133 <mehdikarbitou@gmail.com>
2026-09-01 15:42:28 +0100

    exercice 3

---
+++ exercice_03.js
@@ -0,0 +1,21 @@
+for (let i = 10 ; i > 0; i--) {
+    console.log(i);
+    
+}
+
+let somme = 0;
+for (let i = 1; i < 100 ; i++) {
+
+  somme = somme += i
+}
+ console.log( "la somme " + somme);
+
+
+for (let i = 1; i <= 20; i++) {
+   
+    if (i % 2 === 0) {
+        console.log(i);    
+    }
+}
+
+
