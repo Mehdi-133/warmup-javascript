6d70a2d241fe61fe5ecdf7fff013ed6faa4fbc77
Mehdi-133 <mehdikarbitou@gmail.com>
2026-09-01 14:58:18 +0100

    exercice 1

---
+++ exercice_01.js
@@ -0,0 +1,37 @@
+const  firstName = "mehdi";
+const  lastName = "karbitou";
+const ville = "casablanca " ;
+let  age = "23";
+let enFormation = true;
+
+
+function concat()
+{
+    return firstName +  " "  + lastName + "," + " " +  age + "ans a " + ville  
+}
+function checkAge(){
+    if (age >= 18) {
+        return "status" + " " +  ":" + "adult";
+    }
+    else{
+        return "not adult"
+    }
+}
+
+function checkFormation(){
+    if (enFormation = true) {
+        return "En formation " +  " " + ":" + true
+    }
+
+    else{
+        return false
+    }
+}
+
+
+console.log(concat());
+console.log(checkAge());
+console.log(checkFormation());
+
+
+
