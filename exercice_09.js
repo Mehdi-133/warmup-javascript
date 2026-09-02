feb4d50461612e90137ecad37e7c8e38e428533b
Mehdi-133 <mehdikarbitou@gmail.com>
2026-09-02 19:33:04 +0100

    feat :  La remise fidelite

---
+++ exercice_09.js
@@ -0,0 +1,45 @@
+const commandes = [
+  { montant: 150, statut: "standard" },
+  { montant: 620, statut: "standard" },
+  { montant: 1200, statut: "premium" },
+];
+
+function calculateCommand(montant, statut) {
+  let remisePersent = 0;
+
+  if (montant < 200) {
+    remisePersent = 0;
+  }
+  if (montant >= 200 && montant < 500) {
+    remisePersent = 0.05;
+  }
+  if (montant >= 500 && montant < 1000) {
+    remisePersent = 0.1;
+  }
+  if (montant >= 1000) {
+    remisePersent = 0.15;
+  }
+
+  let remise = montant * remisePersent;
+
+  if (statut === "premium") {
+    remise = remise + 5;
+  }
+
+  const afterRemise = montant - remise;
+
+  const livraison = montant > 300 ? 0 : 30;
+
+  const totalPayer = afterRemise + livraison;
+  return {
+    montant,
+    statut,
+    remisePersent,
+    remise,
+    afterRemise,
+    livraison,
+    totalPayer,
+  };
+}
+
+console.log(calculateCommand(620, "premium"));
